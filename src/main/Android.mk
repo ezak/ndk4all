e5afe57c873837c9ad7e1774f67df442cab89437
ezak <izak.java@gmail.com>
2020-09-22 22:03:07 +0300

    modularize

--- src/main/Android.mk
+++ src/main/Android.mk
@@ -5,9 +5,6 @@ LOCAL_MODULE:= compute
 LOCAL_C_INCLUDES:= ${BOOST_INCLUDE_PATH}
 LOCAL_SRC_FILES:= compute.c
 #LOCAL_SRC_FILES:= thread.cpp
-#LOCAL_STATIC_LIBRARIES:= libboost_thread
-#LOCAL_STATIC_LIBRARIES:= libcryptopp
-#LOCAL_STATIC_LIBRARIES:= libblake2
-LOCAL_STATIC_LIBRARIES:= libargon2
+LOCAL_STATIC_LIBRARIES:= libboost_thread libblake2 libcryptopp libargon2
 
 include $(BUILD_EXECUTABLE)
